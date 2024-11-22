50de832ba41cb5ff337797a4e04229533b787209
Muhiddinovich <mullagoziev@gmail.com>
2024-11-22 01:55:39 -0500

    retrieveTodo and deleteTodo REST API

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -2,6 +2,8 @@ package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +17,19 @@ public class TodoResource {
 		this.todoService = todoService;
 	}
 
-
 	@GetMapping("/users/{username}/todos")
 	public List<Todo> retrieveTodos(@PathVariable String username) {
 		return todoService.findByUsername(username);
 	}
 
+	@GetMapping("/users/{username}/todos/{id}")
+	public Todo retrieveTodo(@PathVariable int id) {
+		return todoService.findById(id);
+	}
+
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable int id) {
+		todoService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
 }
